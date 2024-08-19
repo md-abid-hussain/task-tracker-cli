f44e0ad92b3cb0ab2cd8a60560370f185d8ff363
Md Abid Hussain <abid0hussain1733@gmail.com>
2024-08-19 17:06:04 +0530

    updated jest config

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,7 @@
 module.exports = {
     verbose: true,
-    testMatch: ['<rootDir>/tests/**/*.test.ts'], // Ensure the correct path for test files
+    testMatch: ['<rootDir>/tests/**/*.test.ts'],
     preset: 'ts-jest',
     testEnvironment: 'node',
-    moduleFileExtensions: ['ts', 'js'], // Allow Jest to recognize TypeScript and JavaScript files
-    globals: {
-        'ts-jest': {
-            tsconfig: 'tsconfig.json', // Point to your tsconfig.json file
-        },
-    }
+    moduleFileExtensions: ['ts', 'js'],
 };
